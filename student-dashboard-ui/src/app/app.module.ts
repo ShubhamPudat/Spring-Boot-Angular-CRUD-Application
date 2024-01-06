d7c0de7a89aae5acbe7ea3a824837cc6194fe06e
ShubhamPudat <shubhampudat18@gmail.com>
2024-01-06 15:01:08 +0530

    initial commit

---
+++ student-dashboard-ui/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NgbModule,
+  
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
